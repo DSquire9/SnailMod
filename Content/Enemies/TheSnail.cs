775a21c4002c44ffbdd8d8c1dd838854fd88c2f1
DSquire9 <dss9593@rit.edu>
2025-02-27 16:17:54 -0500

    Added the sprite and anim frames

--- Content/Enemies/TheSnail.cs
+++ Content/Enemies/TheSnail.cs
@@ -14,13 +14,18 @@ namespace SnailMod.Content.Enemies
 {
     public class TheSnail : ModNPC
     {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 6;
+        }
+
         public override void SetDefaults()
         {
             // Change stats
             NPC.immortal = true;
             NPC.width = 18;
-            NPC.height = 20;
-            NPC.damage = 24;
+            NPC.height = 14;
+            NPC.damage = 0;
             NPC.lifeMax = 5;
             NPC.knockBackResist = 0.5f;
             NPC.aiStyle = NPCID.Snail;
